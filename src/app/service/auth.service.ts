6e08707a4fafe9071528dbad87d36568ff225028
JoelMoraes <juniormoraes00@live.com>
2022-01-27 15:18:56 -0300

    Componente tema sem consumo

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpClientModule } from "@angular/common/http";
 
 import { Injectable } from '@angular/core';
 import { Observable } from "rxjs";
+import { environment } from "src/environments/environment.prod";
 import { Usuario } from "../model/Usuario";
 
 import { UsuarioLogin } from "../model/UsuarioLogin";
@@ -15,12 +16,22 @@ export class AuthService {
 
   constructor(
     private http: HttpClient
-) { }
+  ) { }
 
-  entrar(UsuarioLogin: UsuarioLogin): Observable<UsuarioLogin>{
+  entrar(UsuarioLogin: UsuarioLogin): Observable<UsuarioLogin> {
     return this.http.post<UsuarioLogin>('http://localhost:8080/usuarios/logar', UsuarioLogin)
   }
-  cadastrar(Usuario:Usuario): Observable<Usuario>{
-    return this.http.post<Usuario>('http://localhost:8080/usuarios/cadastrar',Usuario)
+  cadastrar(Usuario: Usuario): Observable<Usuario> {
+    return this.http.post<Usuario>('http://localhost:8080/usuarios/cadastrar', Usuario)
+  }
+  logado() {
+    let ok: boolean = false
+
+    if (environment.token != '') {
+      ok = true
+    }
+
+    return ok
+
   }
 }
